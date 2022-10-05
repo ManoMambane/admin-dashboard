54641fad059bd78dedbcc306b4c688e39c53caf8
Mano Mambane <manomambane05@gmail.com>
2022-10-05 09:15:42 +0200

    Chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,8 +1,41 @@
 import './chart.scss'
+// import React, { PureComponent } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+
+const data = [
+  { name: "January", Total: 1200 },
+  { name: "February", Total: 2100 },
+  { name: "January", Total: 800 },
+  { name: "April", Total: 1600 },
+  { name: "May", Total: 900 },
+  { name: "June", Total: 170 },
+];
+
 
 function Chart() {
   return (
-    <div className='chart'>Chart</div>
+    <div className='chart'>
+      <div className="title">Last 6 Months (Revenue)</div>
+      <ResponsiveContainer width="100%" aspect={2 /1}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{
+            top: 10,
+            right: 38,
+            left: 0,
+            bottom: 8
+          }}>
+          <CartesianGrid strokeDasharray="3 3" className='chartGrid'/>
+          <XAxis dataKey="name" stroke='gray'/>
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="Total" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
