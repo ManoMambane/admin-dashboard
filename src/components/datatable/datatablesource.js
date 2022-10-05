fe8aedbdf1e93bb9a632869916a47515a9b455f5
Mano Mambane <manomambane05@gmail.com>
2022-10-05 20:46:51 +0200

    Data Table

---
+++ src/components/datatable/datatablesource.js
@@ -0,0 +1,71 @@
+export const userColumns = [
+    {field: "id", headerName: "ID", width: 70},
+    {field: "user", headerName: "User", width: 230,
+        renderCell: (params) =>
+            {return(
+            <div className="cellWithImg">
+                <img className="cellImg" src={params.row.img} alt="avatar" />
+                {params.row.username}
+            </div>
+            );
+            },
+        },
+        {field: "email", headerName: "Email", width: 230},
+        {field: "age", headerName: "Age", width: 100},
+        {field: "status", headerName: "Status", width: 160, 
+        renderCell:(params)=>{
+            return(
+                <div className={`cellWithStatus ${params.row.status}`}>{params.row.status}</div>
+
+            )
+        }},
+];
+
+//temporary data
+
+export const userRows = [
+    {
+        id: 1,
+        username: "Snow",
+        img: "https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1664876446~exp=1664877046~hmac=5f17fec9cfafd1dcd03f565552f632e47636d5231cecebb459f48859317c31cc",
+        status: "pending",
+        email: "1snow@gmail.com",
+        age: 35,
+    },
+
+    {
+        id: 2,
+        username: "Mo",
+        img: "https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1664876446~exp=1664877046~hmac=5f17fec9cfafd1dcd03f565552f632e47636d5231cecebb459f48859317c31cc",
+        status: "active",
+        email: "1mo@gmail.com",
+        age: 13,
+    },
+
+    {
+        id: 3,
+        username: "Jack",
+        img: "https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1664876446~exp=1664877046~hmac=5f17fec9cfafd1dcd03f565552f632e47636d5231cecebb459f48859317c31cc",
+        status: "passive",
+        email: "jack@gmail.com",
+        age: 55,
+    },
+
+    {
+        id: 1,
+        username: "Bless",
+        img: "https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1664876446~exp=1664877046~hmac=5f17fec9cfafd1dcd03f565552f632e47636d5231cecebb459f48859317c31cc",
+        status: "active",
+        email: "bless@gmail.com",
+        age: 21,
+    },
+
+    {
+        id: 1,
+        username: "jade",
+        img: "https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1664876446~exp=1664877046~hmac=5f17fec9cfafd1dcd03f565552f632e47636d5231cecebb459f48859317c31cc",
+        status: "pending",
+        email: "jade22@gmail.com",
+        age: 77,
+    },
+]
