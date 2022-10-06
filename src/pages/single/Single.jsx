75f23c295bf8149358d03b64669bd79aed8bebd3
Mano Mambane <manomambane05@gmail.com>
2022-10-06 09:53:12 +0200

    Single

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import './single.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+import Chart from '../../components/chart/Chart'
+import List from '../../components/table/Table'
 
 function Single() {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <Sidebar />
+      <div className='singleContainer'>
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img src="https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1664876446~exp=1664877046~hmac=5f17fec9cfafd1dcd03f565552f632e47636d5231cecebb459f48859317c31cc"
+              alt=""
+              className='itemImg'
+              />
+              <div className="details">
+                <h1 className="itemTitle">Mano Mano</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email</span>
+                  <span className="itemValue">mano@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone</span>
+                  <span className="itemValue">+2778342342</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address</span>
+                  <span className="itemValue">Mbomela</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country</span>
+                  <span className="itemValue">SA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3/1} title="User Spending ( Last 6 months)" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List/>
+        </div>
+      </div>
+    </div>
   )
 }
 
